6f7cd4ad411eca49bca58835e3ba71aec6441c88
sanding28 <rachmatyasin09@gmail.com>
2022-09-26 21:24:54 +0700

    update masih error

--- coba_echo/main.go
+++ coba_echo/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -34,10 +35,28 @@ func getProduct(c echo.Context) error{
 	return c.JSON(http.StatusOK, product)
 }
 
+// func updateProduct( c echo.Context) error{
+// 	n := new(Product)
+// 	if err := c.Bind(n); err != nil {
+// 		return err
+// 	}
+// 	ID, _ := strconv.Atoi(c.Param("id"))
+// 	product[ID].Name = n.Name
+// 	return c.JSON(http.StatusOK, product[ID])
+// }
+
+func deleteProduct(c echo.Context) error {
+	ID, _  := strconv.Atoi(c.Param("id"))
+	delete(product, ID)
+	return c.NoContent(http.StatusNoContent)  
+}
+
 func main(){
 	e := echo.New()
 	e.POST("/", CreateProduct)
 	e.GET("/products", getProduct)
+	// e.PUT("/products/:id" , updateProduct)
+	e.DELETE("/products/:id", deleteProduct)
 	e.Logger.Fatal(e.Start(":1323"))
 
 }
